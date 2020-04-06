cae0c261dcab6f30a1805e1c475d47c2f5eacec0
失之时间 <49801226+SwordJack@users.noreply.github.com>
2020-04-07 00:23:38 +0800

    添加“操作符的本质”
    1、创建了“C#语言入门详解”目录，学习刘铁猛的“C#语言入门详解”课程；
    2、新建“操作符详解”解决方案及其下“操作符本质”项目。

---
+++ C#语言入门详解/操作符详解/操作符的本质/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 操作符的本质
+{
+    /// <summary>
+    /// 因“给我一个支点，我就能撬动地球”（阿基米德）这句话，而引申出“给我一个姑娘，我就能创造一个名字”的笑谈。
+    /// 借此笑谈，创建一个Person类，并编写“创造一个民族”的方法。
+    /// </summary>
+    class Person
+    {
+        public string Name;     //人的名字
+
+
+        /// <summary>
+        /// 两个人结婚，然后就可以创造民族了。
+        /// </summary>
+        /// <param name="p1">婚姻中的一方</param>
+        /// <param name="p2">婚姻中的另一方</param>
+        /// <returns></returns>
+        public static List<Person> GetMarry(Person p1, Person p2)
+        {
+            List<Person> people = new List<Person>();   //新建一个Person类的列表people。
+
+            people.Add(p1);     //先把婚姻双方加进去。
+            people.Add(p2);
+            for (int i = 0; i < 11; i++)    //不妨生一个足球队（一个足球队11个人上场）。
+            {
+                Person child = new Person();
+                child.Name = p1.Name + " & " + p2.Name + "'s child";     //孩子的名字姑且就叫“p1和p2的孩子”。
+                people.Add(child);  //将孩子加入people列表。
+            }
+            return people;
+        }
+
+        /// <summary>
+        /// 此处创建操作符“+”，替代静态函数GetMarry，实现两人结婚然后创造民族。
+        /// 此处为偷懒，通过调用GetMarry方法实现操作符的执行。在操作符的花括号内编写函数体亦是可以的。
+        /// </summary>
+        /// <param name="p1">婚姻中的一方</param>
+        /// <param name="p2">婚姻中的另一方</param>
+        /// <returns></returns>
+        public static List<Person> operator +(Person p1, Person p2)
+        {
+            return GetMarry(p1, p2);
+        }
+    }
+}
---
+++ C#语言入门详解/操作符详解/操作符的本质/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 操作符是函数、算法、运算的简记法，操作符依赖于数据类型（比如整数的“/”与浮点数的“/”是不同的除号）。
+/// 但是从感性认识的角度而言，操作符的本质对我们依旧相对模糊。
+/// C#中常用的操作符都是C#语言为我们准备好，即Microsoft的程序员为我们预先编写的。
+/// 故而，本例中，将通过创建一个类型，并为此类型创建一个操作符，以使得我们能够清晰理解操作符的本质。
+/// </summary>
+namespace 操作符的本质
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();  //声明第一个人，命名为Deer。
+            person1.Name = "Deer";
+
+            Person person2 = new Person();  //声明第二个人，命名为May。
+            person2.Name = "May";
+
+            //List<Person> nation = Person.GetMarry(person1, person2);    //声明一个Person类列表nation（民族），让两人结婚。
+
+            List<Person> nation = person1 + person2;    //通过“+”操作符，实现了与调用GetMarry方法一样的效果。
+
+            foreach (var person in nation)  //输出“民族”中每个人的名字。
+            {
+                Console.WriteLine(person.Name);
+            }
+
+        }
+    }
+}
